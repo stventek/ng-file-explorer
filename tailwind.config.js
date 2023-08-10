7d5e97796252b0780d8a3027b584a5592792008b
stventek <Xenizs@protonmail.com>
2023-08-10 12:59:50 -0400

    multi-line truncation for files and folders, fix spacing issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,6 @@ module.exports = {
   daisyui: {
     themes: ["light", "dark", "cyberpunk"],
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require('@tailwindcss/line-clamp')],
 }
 
